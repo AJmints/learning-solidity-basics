3308004b6ec8bf91f0373ae41079317cfcec85cf
AJmints <alexjmints@gmail.com>
2023-09-22 17:42:38 -0500

    added yarn, updated scripts, updated LearningSolidity.txt with recent changes

---
+++ components/ethers-simple-storage/SimpleStorage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+    
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public peopleList;
+
+    /* This store function is the example for the override - inheritence example */
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        peopleList.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber; 
+    }
+
+
+}
