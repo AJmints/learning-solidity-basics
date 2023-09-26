0ac614c48704af35382934bb3f673257e7a099cb
AJmints <alexjmints@gmail.com>
2023-09-25 23:06:56 -0500

    Learning hardhat!

---
+++ components/hardhat-simple-storage/scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat"
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory(
+    "SimpleStorage"
+  )
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.getDeployedCode()
+  console.log(`Deployed contract to: ${simpleStorage.target}`)
+}
+
+main().then(() => process.exit(0)).catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
