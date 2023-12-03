a722eb20b4df76b1cb5308188fffb4212d63303a
AJmints <alexjmints@gmail.com>
2023-12-02 21:17:58 -0600

    added examples of event and emit in hardhat-simple-storage/contracts/SimpleStorage.sol

--- components/hardhat-simple-storage/contracts/SimpleStorage.sol
+++ components/hardhat-simple-storage/contracts/SimpleStorage.sol
@@ -5,6 +5,14 @@ contract SimpleStorage {
 
     uint256 favoriteNumber;
     
+    /* the event keywork that is paired with the emit keyword */
+    event storedNumber(
+        uint256 indexed oldNumber,
+        uint256 indexed newNumber,
+        uint256 addedNumber,
+        address sender
+    );
+    
     mapping(string => uint256) public nameToFavoriteNumber;
 
     struct People {
@@ -16,6 +24,14 @@ contract SimpleStorage {
 
     /* This store function is the example for the override - inheritence example */
     function store(uint256 _favoriteNumber) public virtual {
+        /* Now in smartcontract lottery lesson and just coverd the emit keyword. 
+        Updated this function to save an example */
+        emit storedNumber(
+            favoriteNumber,
+            _favoriteNumber,
+            _favoriteNumber + favoriteNumber,
+            msg.sender
+        );
         favoriteNumber = _favoriteNumber;
     }
 
